0a119496aee249cbf82b80f3b201c47191c939d6
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2016-07-26 11:47:51 +0200

    Load google map with api key

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
-System.register(['@angular/platform-browser-dynamic', '@angular/core', '@angular/http', './components/app.component', 'angular2-google-maps/core'], function(exports_1, context_1) {
+System.register(['@angular/platform-browser-dynamic', '@angular/core', '@angular/http', './components/app.component', 'angular2-google-maps/core', './data/config'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var platform_browser_dynamic_1, core_1, http_1, app_component_1, core_2;
+    var platform_browser_dynamic_1, core_1, http_1, app_component_1, core_2, config_1;
     return {
         setters:[
             function (platform_browser_dynamic_1_1) {
@@ -18,12 +18,21 @@ System.register(['@angular/platform-browser-dynamic', '@angular/core', '@angular
             },
             function (core_2_1) {
                 core_2 = core_2_1;
+            },
+            function (config_1_1) {
+                config_1 = config_1_1;
             }],
         execute: function() {
             core_1.enableProdMode();
             platform_browser_dynamic_1.bootstrap(app_component_1.ConferenceApp, [
                 http_1.HTTP_PROVIDERS,
-                core_2.GOOGLE_MAPS_PROVIDERS]);
+                core_2.GOOGLE_MAPS_PROVIDERS,
+                core_1.provide(core_2.LazyMapsAPILoaderConfig, { useFactory: function () {
+                        var mapConfig = new core_2.LazyMapsAPILoaderConfig();
+                        mapConfig.apiKey = config_1.APPCONFIG.mapApi;
+                        return mapConfig;
+                    } })
+            ]);
         }
     }
 });
--- client/main.ts
+++ client/main.ts
@@ -1,11 +1,18 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
-import {enableProdMode} from '@angular/core';
+import {enableProdMode, provide} from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {ConferenceApp} from './components/app.component';
-import {GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
+import {GOOGLE_MAPS_PROVIDERS, LazyMapsAPILoaderConfig} from 'angular2-google-maps/core';
+import {APPCONFIG} from './data/config';
 
 enableProdMode();
 
 bootstrap(ConferenceApp, [
   HTTP_PROVIDERS,
-  GOOGLE_MAPS_PROVIDERS]);
+  GOOGLE_MAPS_PROVIDERS,
+  provide(LazyMapsAPILoaderConfig, {useFactory: () => {
+    let mapConfig = new LazyMapsAPILoaderConfig();
+    mapConfig.apiKey = APPCONFIG.mapApi;
+    return mapConfig;
+  }})
+]);
