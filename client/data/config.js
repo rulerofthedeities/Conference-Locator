4e5083188c61577aa5282b921f70414133b4a7b6
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2016-07-30 14:22:58 +0200

    Updated gitignore

---
+++ client/data/config.js
@@ -0,0 +1,14 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var APPCONFIG;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("APPCONFIG", APPCONFIG = {
+                mapApi: 'AIzaSyBcs2F4n2aPZOl7DIzSBUXx0xOfY3RK9A4'
+            });
+        }
+    }
+});
+//# sourceMappingURL=config.js.map
---
+++ client/data/config.ts
@@ -0,0 +1,7 @@
+interface AppConfig {
+    mapApi: string;
+}
+
+export const APPCONFIG: AppConfig = {
+  mapApi:'AIzaSyBcs2F4n2aPZOl7DIzSBUXx0xOfY3RK9A4'
+};
